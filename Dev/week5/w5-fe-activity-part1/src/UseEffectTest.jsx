f185a89317931c7c610118e0fe9ef7a4b9222c2d
Saga <sagalehtonen@gmail.com>
2026-09-19 19:31:12 +0300

    week5 added

---
+++ Dev/week5/w5-fe-activity-part1/src/UseEffectTest.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const UseEffectTest = () => {
+    const [toggleOne, setToggleOne] = useState(false);
+    const [toggleTwo, setToggleTwo] = useState(false);
+    useEffect(() => {
+        console.log('UseEffect1 Ran');
+    }, []);
+    useEffect(() => {
+        console.log('UseEffect2 Ran');
+        if (toggleTwo)
+            console.log('toggleTwo slice of state is true so this code runs');
+    }, [toggleTwo]);
+
+  
+  return (
+    
+    <div>
+      {console.log('rendered or re-rendered')}
+      <h1>UseEffectTest Component</h1>
+      <button onClick={() => setToggleOne(!toggleOne)}>ToggleOne</button>
+      <button onClick={() => setToggleTwo(!toggleTwo)}>toggleTwo</button>
+    </div>
+  );
+};
+
+
+export default UseEffectTest;
